82c3f06ce5035d304f32b5567f4622a1d896eca1
Taurus0205 <dolchin1990@mail.ru>
2021-07-13 09:45:57 +0500

    Remove word so'm form convert-info

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const elHumbergerBtn = document.querySelector(".hamburger-btn");
-const elHeader = document.querySelector(".header");
+var usd = 10630.68;
+var rub = 142.96;
+var euro = 12624.26;
 
-elHumbergerBtn.addEventListener("click", function () {
-  elHeader.classList.toggle("header--adoptive");
+var elForm = document.querySelector(".form");
+var userBalance = elForm.querySelector(".form__input");
+var convertInfo = elForm.querySelector(".convert-info");
+var currency = elForm.querySelector(".select");
+
+console.log(currency.value);
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  convertInfo.textContent = userBalance.value / currency.value;
 });

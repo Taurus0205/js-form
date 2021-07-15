068e745f074960c56ed5bafeec8fa7e936f178c0
Taurus0205 <dolchin1990@mail.ru>
2021-07-15 12:00:09 +0500

    Add eraly return codes to js file

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-var usd = 10630.68;
-var rub = 142.96;
-var euro = 12624.26;
+var usd = Number(10630.68);
+var rub = Number(142.96);
+var euro = Number(12624.26);
 
 var elForm = document.querySelector(".form");
-var userBalance = elForm.querySelector(".form__input");
-var convertInfo = elForm.querySelector(".convert-info");
-var currency = elForm.querySelector(".select");
-
-console.log(currency.value);
+var elUserBalance = elForm.querySelector(".form__input");
+var elConvertInfo = elForm.querySelector(".convert-info");
+var elCurrency = elForm.querySelector(".select");
 
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  convertInfo.textContent = userBalance.value / currency.value;
+  var userBalance = Number(elUserBalance.value.trim());
+
+  if (userBalance <= 0 || isNaN(userBalance)) {
+    elUserBalance.classList.add("form__input-alert");
+    elConvertInfo.textContent = "Ma'lumot xato";
+    return;
+  } else {
+    elUserBalance.classList.remove("form__input-alert");
+  }
+
+  if (elCurrency.value === "usd") {
+    elConvertInfo.textContent = (userBalance / usd).toFixed(2);
+  } else if (elCurrency.value === "rubl") {
+    elConvertInfo.textContent = (userBalance / rub).toFixed(2);
+  } else {
+    elConvertInfo.textContent = (userBalance / euro).toFixed(2);
+  }
 });
